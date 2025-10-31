89d4a5d25f8a7d319484e28b715636e5ff39f5eb
Alberte-Remmer <alre0003@stud.kea.dk>
2025-10-31 11:06:25 +0100

    Enhance ColorButton and ColorButtonList components for active state management; add border styling for active buttons in CSS

--- src/app/components/ui/buttons/ColorButtonList.jsx
+++ src/app/components/ui/buttons/ColorButtonList.jsx
@@ -1,7 +1,7 @@
 import ColorButton from "./ColorButton";
 
 // ShiftColor modtager et array af farver som prop
-const ColorButtonList = ({ colors, images, handleProductColor }) => (
+const ColorButtonList = ({ colors, images, productColor, handleProductColor }) => (
   // Container til cirkler og linjer
   <div
     style={{
@@ -14,7 +14,7 @@ const ColorButtonList = ({ colors, images, handleProductColor }) => (
     {colors.map((color, i) => (
       <div key={color}>
         {/* Viser en cirkel med den aktuelle farve */}
-        <ColorButton color={color} src={images[i].src} handleProductColor={handleProductColor} /> {/* Key er en prop i React som identificerer hvert element i en liste*/}
+        <ColorButton color={color} isActive={images[i].src === productColor} src={images[i].src} handleProductColor={handleProductColor} /> {/* Key er en prop i React som identificerer hvert element i en liste*/}
         {/* Tilføjer en lodret linje efter hver cirkel, undtagen den sidste */}
         {i < colors.length - 1 && (
           <div
